68011451423370a96983c3a69dce060e736239a6
hiidz <33335872+hiidz@users.noreply.github.com>
2024-02-15 16:09:57 +0800

    feat: allow interface to connect to multiple clients

--- app/classes/routerInterface.py
+++ app/classes/routerInterface.py
@@ -18,38 +18,44 @@ class RouterInterface:
         self.interface_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.interface_socket.bind((HOST, interface_port))
 
-    def handle_connection(self, conn, address, arp_table):
+
+    def listen(self, conn, address):
         print(f"Connection from {address} established.")
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print(f"Connection from {address} closed.")
-                break
-            print(f"Received data from {address}: {data.decode()}")
-            conn.send(bytes("hello", "utf-8"))
-            arp_table.add_record("ip-sample-2", "mac-sample-2")
-            print(arp_table.get_arp_table())
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print(f"Connection from {address} closed.")
+                    break
+                print(f"Received data from {address}: {data.decode()}")
+                conn.send(bytes("hello", "utf-8"))
+                self.arp_table.add_record("ip-sample-2", "mac-sample-2")
+                print(self.arp_table.get_arp_table())
+        except ConnectionResetError:
+            print(f"Connection with {address} closed.")
+            return
 
-        print(f"Connection with {address} closed.")
-        conn.close()
-
-    def listen(self):
-        self.interface_socket.listen(5)
-
-        print(f"Interface on {HOST}:{self.interface_port} waiting for connections...")
-
-        while True:
-            conn, address = self.interface_socket.accept()
-            self.handle_connection(conn, address, self.arp_table)
 
     def handle_input(self):
         while True:
             command_input = input()
 
+
+    def multi_listen_handler(self):
+        self.interface_socket.listen()
+
+        while True:
+            conn, address = self.interface_socket.accept()
+
+            # For each new connection, create a new thread to continue listening
+            threading.Thread(target=self.listen, args=(conn, address)).start()
+
+
     def start(self):
         try:
-            threading.Thread(target = self.listen).start()
+            # A thread that will listen for new incoming connections
+            threading.Thread(target = self.multi_listen_handler).start()
             self.handle_input()
 
         except KeyboardInterrupt:
