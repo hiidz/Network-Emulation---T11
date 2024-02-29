46a7c4065a3068cc54232ead2271c7bd1eba415b
YuanSheng <chongyuansheng@gmail.com>
2024-02-29 08:04:28 +0800

    Fix: Updated import issues in firewall.py

--- app/classes/firewall.py
+++ app/classes/firewall.py
@@ -1,9 +1,22 @@
 from typing import Dict, List
-from firewallUtil import print_brk, input_ip_sequence
+import os
+
+
+def print_brk():
+    print('-' * os.get_terminal_size().columns)
+
+
+def input_ip_sequence(prompt: str) -> str:
+    ip_to_add = input(prompt)
+    valid_input = True if ip_to_add[:2] == "0x" else False
+    while not valid_input:
+        ip_to_add = input("Invalid input, please enter a valid IP (e.g., 0x1A).\n> ")
+        valid_input = True if ip_to_add[:2] == "0x" else False
+
+    return ip_to_add
 
 
 class Firewall:
-
     blacklist: List[str]
     whitelist: List[str]
     blacklist_disabled: bool
